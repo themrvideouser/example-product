a09e96f5a65c94f6e49292834168a84087f7b792
themrvideouser <frederik.gschaider@gmail.com>
2019-10-03 13:39:14 +0200

    initial commit

---
+++ api.py
@@ -0,0 +1,23 @@
+# Product Service
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+    def get(self):
+        return {
+            'products': [
+                'Erdbeeren',
+                'Äpfel',
+                'Nüsse',
+                'Birne',
+                'Rumkugeln',
+            ]
+        }
+api.add_resource(Product, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
